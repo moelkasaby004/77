c2ec72e9f832a1fb19f62ee739fb3c5d2a3a2f6d
moelkasaby004 <153857316+moelkasaby004@users.noreply.github.com>
2024-02-22 23:38:40 +0200

    Update playercollision.cs
    S

--- Assets/playercollision.cs
+++ Assets/playercollision.cs
@@ -13,8 +13,8 @@ if(clold.collider.tag=="obstacle"){
    // Debug.Log("we hit an obstacle");
   // rr.endgame();
 
-//FindObjectOfType<game>().EndGame();
-//pl.enabled=false;
+FindObjectOfType<game>().EndGame();
+pl.enabled=false;
 }
      }
 
